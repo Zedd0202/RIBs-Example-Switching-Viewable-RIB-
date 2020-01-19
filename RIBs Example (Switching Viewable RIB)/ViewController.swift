18a9a661ba990eafa891a35b19aa4e49d2379b5f
Zedd <zedd.0202@kakaocorp.com>
2020-01-19 16:22:53 +0900

    first commit

---
+++ RIBs Example (Switching Viewable RIB)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RIBs Example (Switching Viewable RIB)
+//
+//  Created by Zedd on 2020/01/19.
+//  Copyright © 2020 Zedd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
