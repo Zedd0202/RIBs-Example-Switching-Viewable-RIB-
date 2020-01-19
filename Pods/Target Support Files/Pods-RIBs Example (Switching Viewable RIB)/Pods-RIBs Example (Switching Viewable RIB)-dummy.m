7ad91ab950b83f1c6c347147db5a0779aa4790b6
Zedd0202 <ch41150@hanmail.net>
2020-01-19 16:27:45 +0900

    root 설정:

---
+++ Pods/Target Support Files/Pods-RIBs Example (Switching Viewable RIB)/Pods-RIBs Example (Switching Viewable RIB)-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RIBs_Example__Switching_Viewable_RIB_ : NSObject
+@end
+@implementation PodsDummy_Pods_RIBs_Example__Switching_Viewable_RIB_
+@end
--- RIBs Example (Switching Viewable RIB)/SceneDelegate.swift
+++ RIBs Example (Switching Viewable RIB)/SceneDelegate.swift
@@ -1,25 +1,37 @@
 //
 //  SceneDelegate.swift
-//  RIBs Example (Switching Viewable RIB)
+//  SceneDelegate + RIBs
 //
-//  Created by Zedd on 2020/01/19.
+//  Created by Zedd on 2020/01/14.
 //  Copyright © 2020 Zedd. All rights reserved.
 //
 
 import UIKit
+import RIBs
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    private var launchRouter: LaunchRouting?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            self.window = window
+
+            let launchRouter = RootBuilder(dependency: AppComponent()).build()
+            self.launchRouter = launchRouter
+            launchRouter.launchFromWindow(window)
+        }
     }
 
+    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
@@ -47,7 +59,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
+}
+class AppComponent: Component<EmptyDependency>, RootDependency {
 
-
+    init() {
+        super.init(dependency: EmptyComponent())
+    }
 }
 
